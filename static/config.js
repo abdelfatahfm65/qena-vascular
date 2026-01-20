6bba8897795b5acfa846c47e2031e399e963fba2
abdelfatahfm65 <abdelfatahfm65@gmail.com>
2026-01-20 18:22:54 +0200

    configstatic adding

---
+++ static/config.js
@@ -0,0 +1,5 @@
+window.__APP_CONFIG__ = {
+    SUPABASE_URL: "https://mozoyqxdqypxuztetmtz.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vem95cXhkcXlweHV6dGV0bXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NTcwMDYsImV4cCI6MjA4NDQzMzAwNn0.FsYKJKsRjdv06MNR6_dVpsTOA-GUVUDX4C_E_RPv3-g"
+  };
+  
